2e28267c40bd1647cc2b715732c1fb60f653b008
Adrian Brezoi <adrian.s.brezoi@gmail.com>
2020-10-30 13:40:30 +0100

    Added basic styling/structure for Main & Profile components.

---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+// @ is an alias to /src
+
+import OrderHistory from "@/components/OrderHistory/OrderHistory.vue";
+import Profile from "@/components/Profile/Profile.vue";
+
+export default {
+    name: "Main",
+    components: {
+        Profile,
+        OrderHistory
+    }
+};
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,22 +1,3 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <Profile />
-    <OrderHistory />
-  </div>
-</template>
-
-<script>
-// @ is an alias to /src
-
-import OrderHistory from "@/components/OrderHistory/OrderHistory.vue";
-import Profile from "@/components/Profile/Profile.vue";
-
-export default {
-  name: "Main",
-  components: {
-    Profile,
-    OrderHistory
-  }
-};
-</script>
+<template src="./Main.html"/>
+<script src="./Main.js"/>
+<style scoped lang="scss" src="./Main.scss"/>
