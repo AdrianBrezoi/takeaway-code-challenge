9888289f60f7b9109c543431d8ff70fce45ea356
Adrian Brezoi <adrian.s.brezoi@gmail.com>
2020-10-30 14:16:53 +0100

    Added globally available css (for variables) Added Header component (w/ basic styling)

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
 import NotificationBanner from "@/components/NotificationBanner/NotificationBanner.vue";
+import Header from "@/components/Header/Header.vue";
+
 import axios from "axios";
 
 export default {
-  name: "App",
-  components: { NotificationBanner },
-  mounted: () => {
-    // Fake user
-    axios
-      .get("https://jsonplaceholder.typicode.com/users?_limit=1")
-      .then(value => {
-        console.log("ABR - User Profile", value);
-      });
+    name: "App",
+    components: {Header, NotificationBanner},
+    mounted: () => {
+        // Fake user
+        axios
+            .get("https://jsonplaceholder.typicode.com/users?_limit=1")
+            .then(value => {
+                console.log("ABR - User Profile", value);
+            });
 
-    //Fake order history
-    axios
-      .get("https://jsonplaceholder.typicode.com/posts?_limit=5")
-      .then(value => {
-        console.log("ABR - Order History", value);
-      });
+        //Fake order history
+        axios
+            .get("https://jsonplaceholder.typicode.com/posts?_limit=5")
+            .then(value => {
+                console.log("ABR - Order History", value);
+            });
 
-    // Fake order details
-    axios
-      .get("https://jsonplaceholder.typicode.com/posts/1/comments?_limit=2")
-      .then(value => {
-        console.log("ABR - Order details", value);
-      });
+        // Fake order details
+        axios
+            .get("https://jsonplaceholder.typicode.com/posts/1/comments?_limit=2")
+            .then(value => {
+                console.log("ABR - Order details", value);
+            });
 
-    console.log("HI");
-  }
+        console.log("HI");
+    }
 };
