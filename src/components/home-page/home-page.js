2e3f491383ed0bc5e8edfc121fbbfceda3565c8a
Adrian Brezoi <adrian.s.brezoi@gmail.com>
2020-11-01 18:42:59 +0100

    Added component titles in home-page. Updated styling & added labels for UserProfile component. Updated translations.

--- src/components/home-page/home-page.js
+++ src/components/home-page/home-page.js
@@ -1,10 +1,10 @@
-import Profile from "@/components/home-page/user-profile/user-profile.vue";
+import UserProfile from "@/components/home-page/user-profile/user-profile.vue";
 import { mapGetters } from "vuex";
 
 export default {
   name: "HomePage",
   components: {
-    Profile
+    UserProfile,
   },
   computed: { ...mapGetters(["orders"]) },
   methods: {
