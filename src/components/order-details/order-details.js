802ed4ffc76e56a8c42b3ab48b7f2d4ee945beea
Adrian Brezoi <adrian.s.brezoi@gmail.com>
2020-11-01 20:39:09 +0100

    Updated styling & content of order-details component. Added extra data to order-details with id '1' to partly simulate an active order.

--- src/components/order-details/order-details.js
+++ src/components/order-details/order-details.js
@@ -3,6 +3,11 @@ import axios from "axios";
 export default {
   name: "OrderDetails",
   props: ["orderId"],
+  methods: {
+    backClicked(){
+      this.$router.back();
+    }
+  },
   data() {
     return { orderDetails: {} };
   },
@@ -12,8 +17,11 @@ export default {
         `https://jsonplaceholder.typicode.com/posts/${this.orderId}/comments?_limit=1`
       )
       .then(response => {
-        console.log("Order details DATA for id", response.data, this.orderId);
-        this.orderDetails = response.data[0];
+        if (parseInt(this.orderId) === 1){
+          this.orderDetails = {restaurant: 'Blue Sakura', status: 'preparing', ...response.data[0]};
+        } else {
+          this.orderDetails = response.data[0];
+        }
       });
   }
 };
